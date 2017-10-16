fe06844f57ac7924ee151c91e40d6e6ecc18290d
Andrew Rady <andrew.rady@ritterim.com>
2017-10-15 18:06:13 -0600

    basic api with crud applications

---
+++ MATesting/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MATesting.Models
+{
+    public class Student
+    {
+        public long Id { get; set; }
+        public String Name { get; set; }
+        public String Rank { get; set; }
+        public String Judge { get; set; }  
+        public int FormScore { get; set; }
+        public int WeaponScore { get; set; }
+        public int SparringScore { get; set; }
+        public int BoardBreakScore { get; set; }
+        public int FitTestScore { get; set; }
+
+    }
+}
