9b6176a79559bdb8dc84c016118bfb4e96dff1a2
Andrew Rady <andrew.arsoftware@gmail.com>
2017-10-21 00:58:22 -0600

    clead up router

--- MATesting/wwwroot/src/App.vue
+++ MATesting/wwwroot/src/App.vue
@@ -1,25 +1,38 @@
 <template>
   <div id="app">
-    <router-link :to="{ name: 'Home' }">Home</router-link>
-    <router-link :to="{ name: 'Instructor' }">Instructor</router-link>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">ATA Testing</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/instructor" class="nav-link">Instructor</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'MA Testing',
-  data () {
+  name: "MA Testing",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  },
-}
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -44,5 +57,5 @@ li {
 
 a {
   color: #42b983;
-}
+} */
 </style>
